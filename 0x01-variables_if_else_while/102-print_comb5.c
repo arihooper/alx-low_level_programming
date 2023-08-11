609ea057af4600cc0614c67db4ec6c70cd8fccd6
arihooper <arielsebsib@gmail.com>
2023-08-11 15:43:21 -0700

    wooo numbers is coll

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int num1, num2;
 
 for (num1 = 0; num1 < 100; num1++)
 {
-for (num2 = 0; num2 < 100; num2++)
+for (num2 = num1 + 1; num2 < 100; num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
